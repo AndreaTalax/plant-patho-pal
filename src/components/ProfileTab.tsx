22ad65395d4e4eead286e9eb8114b914e0122135
AndreaTalax <talaiaandrea@gmail.com>
2025-06-04 20:32:05 +0200

    Update ProfileTab.tsx

--- src/components/ProfileTab.tsx
+++ src/components/ProfileTab.tsx
@@ -1,4 +1,4 @@
-
+import Badge from '@mui/material/Badge';
 import { useState, useRef, useEffect } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
