eb901be00a580faf28ea79eb49ee255fc2c91b22
AndreaTalax <talaiaandrea@gmail.com>
2025-06-11 21:16:52 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
 	darkMode: ["class"],
-	content: [
+	text: [
 		"./pages/**/*.{ts,tsx}",
 		"./components/**/*.{ts,tsx}",
 		"./app/**/*.{ts,tsx}",
